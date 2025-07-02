c1374b8f1b85e73a7e62feb723383ba10a1c1120
Wilmer Zambrano <zambranorwilmerh@gmail.com>
2025-07-02 13:55:58 -0400

    nuevos cambios

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import "./Footer.css"
 export const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2023 Mi Tienda - Todos los derechos reservados</p>
+      <p>© 2025 Mi Tienda - Todos los derechos reservados</p>
     </footer>
   )
 }
